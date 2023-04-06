9736c949cdaa47875d2511a28f4dbf74b454641a
arthur.jacquemin <arthurjacquemin2@gmail.com>
2023-04-06 13:52:55 +0200

    fix: dotenv

--- api/index.php
+++ api/index.php
@@ -1,8 +1,12 @@
 <?php
 
 use App\Kernel;
+use Dotenv\Dotenv;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+$dotenv = Dotenv::createImmutable(__DIR__ . "/../");
+$dotenv->load();
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
